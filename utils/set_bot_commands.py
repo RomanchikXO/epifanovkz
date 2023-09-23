b3a4b29a51ed30d32508aa03eb4ec8871fe8f08f
roman_dikarev <super.romanchik228@yandex.ru>
2023-09-23 17:00:56 +0600

    some comment

---
+++ utils/set_bot_commands.py
@@ -0,0 +1,10 @@
+from telebot.types import BotCommand
+from config_data.config import DEFAULT_COMMANDS
+
+
+def set_default_commands(bot):
+    print(f'Дефолтные команды {[BotCommand(*i).command for i in DEFAULT_COMMANDS]}')
+    bot.set_my_commands(
+        [BotCommand(*i) for i in DEFAULT_COMMANDS]
+    )
+    print('Команды загружены\n')
