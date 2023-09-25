da5e95e49e5f1c25402e46dfed0e8299f8c7d111
roman_dikarev <super.romanchik228@yandex.ru>
2023-09-25 20:01:52 +0600

    changed button

--- handlers/default_handlers/start.py
+++ handlers/default_handlers/start.py
@@ -1,13 +1,13 @@
 from telebot.types import Message
-from loader import bot
-from keyboards.reply.
+from epifanov_clinic.loader import bot
+from epifanov_clinic.keyboards.reply.buttoms import create_keyboard_person
 
 
 
 @bot.message_handler(state="*", commands=["start"])
 def bot_start(message: Message) -> None:
     print('Декоратор @bot.message_handler(commands=["start"]) сработал')
-    change_keyboard = create_change_buttom()
-    bot.send_message(message.from_user.id, "Выберите, какие данные нужно изменить:", reply_markup=change_keyboard)
+    change_keyboard = create_keyboard_person()
+    bot.send_message(message.from_user.id, "Выберите, профессию:", reply_markup=change_keyboard)
     bot.reply_to(message, f"Привет, {message.from_user.full_name}!"
                           f"Для вывода справки введите команду /help")
