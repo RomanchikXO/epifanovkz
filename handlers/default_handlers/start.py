4903cf5b8e857eac442838f6c166b55f2cefb46d
Roman <super.romanchik228@yandex.ru>
2023-09-29 00:26:04 +0600

    незначительные изменения

--- handlers/default_handlers/start.py
+++ handlers/default_handlers/start.py
@@ -1,13 +1,14 @@
 from telebot.types import Message
-from epifanov_clinic.loader import bot
-from epifanov_clinic.keyboards.reply.buttoms import create_keyboard_person
-
+from loader import bot
+from keyboards.reply.buttoms import create_keyboard_person
+from states.person_info import UserInfoState
 
 
 @bot.message_handler(state="*", commands=["start"])
 def bot_start(message: Message) -> None:
     print('Декоратор @bot.message_handler(commands=["start"]) сработал')
     change_keyboard = create_keyboard_person()
+    bot.set_state(message.from_user.id, UserInfoState.change_doc, message.chat.id)
     bot.send_message(message.from_user.id, "Выберите, профессию:", reply_markup=change_keyboard)
-    bot.reply_to(message, f"Привет, {message.from_user.full_name}!"
-                          f"Для вывода справки введите команду /help")
+
+
