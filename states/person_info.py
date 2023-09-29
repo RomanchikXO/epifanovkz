a1590d5203ce58a4d4eebf8c159a2d1b3286bac7
roman_dikarev <super.romanchik228@yandex.ru>
2023-09-29 19:56:11 +0600

    some com

--- states/person_info.py
+++ states/person_info.py
@@ -2,7 +2,8 @@ from telebot.handler_backends import State, StatesGroup
 
 class UserInfoState(StatesGroup):
     change_staff = State()
+    change_name = State()
     change_doc = State()
-    change_massage = State()
+    change_mas = State()
     add_info = State()
     viewing_information = State()
