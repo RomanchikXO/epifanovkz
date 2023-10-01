e224567783ac4f9c6dd0508e55d8e107ebddb661
Roman <super.romanchik228@yandex.ru>
2023-10-02 00:15:02 +0600

    добавил состояние change_date убрал выбор состояний для отдельных должностей

--- states/person_info.py
+++ states/person_info.py
@@ -3,7 +3,7 @@ from telebot.handler_backends import State, StatesGroup
 class UserInfoState(StatesGroup):
     change_staff = State()
     change_name = State()
-    change_doc = State()
-    change_mas = State()
     add_info = State()
     viewing_information = State()
+
+    change_date = State()
