7bbd2962c8d2a12e7d25224ed3ba364b6516872c
Roman <super.romanchik228@yandex.ru>
2023-10-02 00:13:17 +0600

    добавил кнопки Добавить/Прочитать

--- keyboards/reply/buttoms.py
+++ keyboards/reply/buttoms.py
@@ -17,6 +17,7 @@ def create_keyboard_adm():
 
     button1 = types.KeyboardButton("Анара")
     button2 = types.KeyboardButton("Кристина")
+
     keyboard.add(button1, button2)
     return keyboard
 
@@ -31,10 +32,21 @@ def create_keyboard_doc():
     keyboard.add(button1, button2, button3)
     return keyboard
 
+
 def create_keyboard_mas():
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
 
     button1 = types.KeyboardButton("Роман")
     button2 = types.KeyboardButton("Луиза")
+    keyboard.add(button1, button2)
+    return keyboard
+
+
+def add_task():
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+    button1 = types.KeyboardButton("Добавить")
+    button2 = types.KeyboardButton("Прочитать")
+
     keyboard.add(button1, button2)
     return keyboard
