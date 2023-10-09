205294bbe0cda75f44a95f0e208e279483d07281
Roman <super.romanchik228@yandex.ru>
2023-10-10 00:38:17 +0600

    теперь в кнопку передается имя пациента

--- keyboards/inline/completed_or_not.py
+++ keyboards/inline/completed_or_not.py
@@ -2,8 +2,8 @@ from telebot import types
 from typing import Tuple
 
 
-def done_for_task() -> Tuple[str, types.InlineKeyboardMarkup]:
+def done_for_task(name: str) -> types.InlineKeyboardMarkup:
     keyboard = types.InlineKeyboardMarkup()
-    confirm_button = types.InlineKeyboardButton("Выполнено", callback_data="confirm")
+    confirm_button = types.InlineKeyboardButton("Выполнено", callback_data=name)
     keyboard.add(confirm_button)
     return keyboard
