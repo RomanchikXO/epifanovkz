c07e1fc478cbf5467462eec656dcedcacca6eee0
Роман Дикарев <super.romanchik228@yandex.ru>
2023-11-07 21:42:41 +0600

    - добавил тайпинг

--- handlers/default_handlers/funcs_for_data/funcs.py
+++ handlers/default_handlers/funcs_for_data/funcs.py
@@ -1,5 +1,7 @@
 from loader import bot
+from telebot.types import Message
 
-def add_name_path(message):
+
+def add_name_path(message: Message) -> None:
     with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
         data['name_pat'] = message.text.title()
