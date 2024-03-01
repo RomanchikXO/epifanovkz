082321db9266d3bc814813b1ba84fe305c02faf8
roman_dikarev <super.romanchik228@yandex.ru>
2024-03-01 19:09:32 +0500

    мультипоточность

--- main.py
+++ main.py
@@ -4,9 +4,17 @@ from utils.set_bot_commands import set_default_commands
 from telebot.custom_filters import StateFilter
 from database import DataBase
 
+import concurrent.futures
+from getter_tasks_everyday import get_tasks_everyday
+
 
 if __name__ == "__main__":
     DataBase.create_tables()
     bot.add_custom_filter(StateFilter(bot))
     set_default_commands(bot)
-    bot.infinity_polling()
+    #используем многопоточность
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.submit(get_tasks_everyday)
+        executor.submit(bot.infinity_polling)
+
+
