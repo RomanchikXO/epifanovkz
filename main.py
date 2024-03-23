ba4e9e80d6dafa7a91ed386230d336fdd6e728ce
roman_dikarev <super.romanchik228@yandex.ru>
2024-03-23 13:55:00 +0500

    some change

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ import handlers  # noqa
 from utils.set_bot_commands import set_default_commands
 from telebot.custom_filters import StateFilter
 from database import DataBase
-
-import concurrent.futures
 from utils.getter_tasks_everyday import get_tasks_everyday
+import threading
+import schedule
+
+
+def schedule_func():
+    weekdays = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
+    for day in weekdays:
+        schedule.every().day.at("07:55").do(get_tasks_everyday)
 
 
 if __name__ == "__main__":
     DataBase.create_tables()
     bot.add_custom_filter(StateFilter(bot))
     set_default_commands(bot)
-    #используем многопоточность
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        executor.submit(get_tasks_everyday)
-        executor.submit(bot.infinity_polling(skip_pending=True))
-
-
+    threading.Thread(target=schedule_func).start()
+    threading.Thread(target=bot.infinity_polling, args=(), kwargs={"skip_pending": True}).start()
